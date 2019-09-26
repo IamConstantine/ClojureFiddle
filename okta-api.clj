876c98be9ff9a9b099f01e13804c290a835e239f
Vishal Shashikumar <vishalskumar12@gmail.com>
2019-09-26 16:07:32 +0700

    updated the get-token okta api

--- okta-api.clj
+++ okta-api.clj
@@ -12,12 +12,11 @@
                :headers      {:authorization (str "SSWS " (okta-config :api-token))}}))
 
 (defn get-token
-  ;required for authorization web flow to retrieve the tokens from the authorization code
   [okta-config code]
-  (let [ (client/post (str "https://" (okta-config :okta-domain) "/oauth2/default/v1/token")
+  (client/post (str "https://" (okta-config :okta-domain) "/oauth2/default/v1/token")
                {:accept       :application/json
                 :content-type :application/x-www-form-urlencoded
-                :basic-auth [(okta-config :client-id) (okta-config :secret)]
+                :basic-auth   [(okta-config :client-id) (okta-config :secret)]
                 :query-params {:grant_type   "authorization_code"
                                :redirect_uri (okta-config :redirect-uri)
-                               :code         code}})]))
+                               :code         code}}))
